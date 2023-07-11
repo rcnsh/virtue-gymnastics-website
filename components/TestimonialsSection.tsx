367a2b272ad79b276a9c109db9236cd8bb5e635a
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-07-11 16:54:32 +0100

    extract logic for instagram and testimonials sections

---
+++ components/TestimonialsSection.tsx
@@ -0,0 +1,104 @@
+import Image from "next/image";
+import styles from "@/styles/TestimonialsSection.module.css";
+import testimonialStyles from "@/styles/testimonialsCarousel.module.css";
+import Link from "next/link";
+import useEmblaCarousel from "embla-carousel-react";
+
+const TestimonialsSection = () => {
+  const [emblaRef] = useEmblaCarousel({
+    startIndex: 2,
+  });
+  return (
+    <section>
+      <div
+        className={
+          "w-[100%] text-center text-[80px] xl:text-[100px] 2xl:text-[120px] uppercase leading-none unselectable"
+        }
+      >
+        <h1 className={styles.testimonialsTitle}>TESTIMONIALS</h1>
+      </div>
+      <h1
+        className={
+          "text-7xl bottom-60 left-28 font-bold relative overflow-hidden unselectable"
+        }
+      >
+        TESTIMONIALS
+      </h1>
+      <div className={styles.leaveAReview}>
+        <Link
+          href={
+            "https://www.google.com/maps/place/Virtue+movement+co./@51.4620734,-1.0170551,17z/data=!4m8!3m7!1s0x48769bbc0776c789:0x26f3bea60b3487ff!8m2!3d51.4620734!4d-1.0144748!9m1!1b1!16s%2Fg%2F11bxfyzhx5"
+          }
+        >
+          <button
+            className={
+              "bg-gray-900 hover:bg-gray-800 text-white font-bold py-5 px-5 rounded bottom-60 relative border-white border-2 hover:border-gray-800 overflow-hidden"
+            }
+          >
+            Leave a review
+          </button>
+        </Link>
+      </div>
+      <div className={styles.viewAllReviews}>
+        <Link
+          href={
+            "https://www.google.com/maps/place/Virtue+movement+co./@51.4620734,-1.0170551,17z/data=!4m8!3m7!1s0x48769bbc0776c789:0x26f3bea60b3487ff!8m2!3d51.4620734!4d-1.0144748!9m1!1b1!16s%2Fg%2F11bxfyzhx5"
+          }
+        >
+          <button
+            className={
+              "bg-gray-900 hover:bg-gray-800 text-white font-bold py-5 px-5 rounded bottom-60 relative border-white border-2 hover:border-gray-800 overflow-hidden"
+            }
+          >
+            View All Reviews
+          </button>
+        </Link>
+      </div>
+      <section className={"relative bottom-64 overflow-hidden"}>
+        <div className={"flex align-middle justify-center"}>
+          <div className={testimonialStyles.embla} ref={emblaRef}>
+            <div className={testimonialStyles.embla__container}>
+              <Image
+                className={testimonialStyles.embla__slide}
+                alt={"testimonial1"}
+                src={"/home/testimonials/testimonial1.png"}
+                width={1000}
+                height={1000}
+              />
+              <Image
+                className={testimonialStyles.embla__slide}
+                alt={"testimonial2"}
+                src={"/home/testimonials/testimonial2.png"}
+                width={1000}
+                height={1000}
+              />
+              <Image
+                className={testimonialStyles.embla__slide}
+                alt={"testimonial3"}
+                src={"/home/testimonials/testimonial3.png"}
+                width={1000}
+                height={1000}
+              />
+              <Image
+                className={testimonialStyles.embla__slide}
+                alt={"testimonial4"}
+                src={"/home/testimonials/testimonial4.png"}
+                width={1000}
+                height={1000}
+              />
+              <Image
+                className={testimonialStyles.embla__slide}
+                alt={"testimonial5"}
+                src={"/home/testimonials/testimonial5.png"}
+                width={1000}
+                height={1000}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default TestimonialsSection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,16 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
-import testimonialStyles from "@/styles/testimonialsCarousel.module.css";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import LineBreaks from "@/components/line-breaks";
-import useEmblaCarousel from "embla-carousel-react";
-import Link from "next/link";
+import InstagramSection from "@/components/InstagramSection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+
 const Index: NextPage = () => {
-  const [emblaRef] = useEmblaCarousel({
-    startIndex: 2,
-  });
   return (
     <>
-      <motion.div
+      <motion.main
         initial={{
           opacity: 0,
         }}
@@ -158,158 +155,12 @@ const Index: NextPage = () => {
             />
             <div className={styles.glow2}></div>
           </div>
-          <div className={"overflow-hidden z-10"}>
-            <h1 className={styles.instagram}>INSTAGRAM</h1>
-            <h1
-              className={
-                "text-6xl bottom-80 left-28 font-bold relative unselectable"
-              }
-            >
-              INSTAGRAM
-            </h1>
-            <motion.img
-              src={"/home/instagram arrow.png"}
-              className={styles.instagramArrow}
-              alt={""}
-              width={100}
-            />
-            <motion.button
-              className={styles.buttonFollow}
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-            >
-              FOLLOW
-            </motion.button>
-          </div>
-          <div className={"container"}>
-            <div className={"flex flex-wrap justify-center"}>
-              <Image
-                src={"/home/promo image 3.png"}
-                alt={""}
-                width={400}
-                height={400}
-                className={`${styles.instagram2} ${styles.promoImages}`}
-              />
-              <Image
-                src={"/home/promo image 1.png"}
-                alt={""}
-                width={400}
-                height={400}
-                className={`${styles.instagram1} ${styles.promoImages}`}
-              />
-              <Image
-                src={"/home/promo image 2.png"}
-                alt={""}
-                width={400}
-                height={400}
-                className={`${styles.instagram3} ${styles.promoImages}`}
-              />
-            </div>
-          </div>
-          <div className={"relative bottom-52"}>
-            <Image
-              src={"/home/banner.png"}
-              alt={""}
-              width={3000}
-              height={3397}
-              className={styles.banner}
-            />
-          </div>
         </div>
-        <div>
-          <div
-            className={
-              "w-[100%] text-center text-[80px] xl:text-[100px] 2xl:text-[120px] uppercase leading-none unselectable"
-            }
-          >
-            <h1 className={styles.testimonialsTitle}>TESTIMONIALS</h1>
-          </div>
-          <h1
-            className={
-              "text-7xl bottom-60 left-28 font-bold relative overflow-hidden unselectable"
-            }
-          >
-            TESTIMONIALS
-          </h1>
-          <div className={styles.leaveAReview}>
-            <Link
-              href={
-                "https://www.google.com/maps/place/Virtue+movement+co./@51.4620734,-1.0170551,17z/data=!4m8!3m7!1s0x48769bbc0776c789:0x26f3bea60b3487ff!8m2!3d51.4620734!4d-1.0144748!9m1!1b1!16s%2Fg%2F11bxfyzhx5"
-              }
-            >
-              <button
-                className={
-                  "bg-gray-900 hover:bg-gray-800 text-white font-bold py-5 px-5 rounded bottom-60 relative border-white border-2 hover:border-gray-800 overflow-hidden"
-                }
-              >
-                Leave a review
-              </button>
-            </Link>
-          </div>
-          <div className={styles.viewAllReviews}>
-            <Link
-              href={
-                "https://www.google.com/maps/place/Virtue+movement+co./@51.4620734,-1.0170551,17z/data=!4m8!3m7!1s0x48769bbc0776c789:0x26f3bea60b3487ff!8m2!3d51.4620734!4d-1.0144748!9m1!1b1!16s%2Fg%2F11bxfyzhx5"
-              }
-            >
-              <button
-                className={
-                  "bg-gray-900 hover:bg-gray-800 text-white font-bold py-5 px-5 rounded bottom-60 relative border-white border-2 hover:border-gray-800 overflow-hidden"
-                }
-              >
-                View All Reviews
-              </button>
-            </Link>
-          </div>
-          <div className={"relative bottom-64 overflow-hidden"}>
-            <div className={"flex align-middle justify-center"}>
-              <div className={testimonialStyles.embla} ref={emblaRef}>
-                <div className={testimonialStyles.embla__container}>
-                  <Image
-                    className={testimonialStyles.embla__slide}
-                    alt={"testimonial1"}
-                    src={"/home/testimonials/testimonial1.png"}
-                    width={1000}
-                    height={1000}
-                  />
-                  <Image
-                    className={testimonialStyles.embla__slide}
-                    alt={"testimonial2"}
-                    src={"/home/testimonials/testimonial2.png"}
-                    width={1000}
-                    height={1000}
-                  />
-                  <Image
-                    className={testimonialStyles.embla__slide}
-                    alt={"testimonial3"}
-                    src={"/home/testimonials/testimonial3.png"}
-                    width={1000}
-                    height={1000}
-                  />
-                  <Image
-                    className={testimonialStyles.embla__slide}
-                    alt={"testimonial4"}
-                    src={"/home/testimonials/testimonial4.png"}
-                    width={1000}
-                    height={1000}
-                  />
-                  <Image
-                    className={testimonialStyles.embla__slide}
-                    alt={"testimonial5"}
-                    src={"/home/testimonials/testimonial5.png"}
-                    width={1000}
-                    height={1000}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </motion.div>
+        <InstagramSection />
+        <TestimonialsSection />
+      </motion.main>
     </>
   );
 };
 
 export default Index;
-
-// TODO reminder to use https://www.embla-carousel.com/get-started/react/
