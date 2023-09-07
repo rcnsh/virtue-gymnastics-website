89f77270eb94041faef912d7594cf870415c169c
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-09-07 11:35:30 +0100

    remove comments

--- pages/api/firebaseConfig.ts
+++ pages/api/firebaseConfig.ts
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyCIze6Z4ERj3_0_u0psJa8TEZh0CwhcGa0',
   authDomain: 'virtue-movement.firebaseapp.com',
