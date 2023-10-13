1753e0a68521561fb70aa68c7f87894ca8a81114
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-10-13 13:12:37 +0100

    update storeUser api route to use prisma instead of postgresql

--- pages/api/store/storeUser.ts
+++ pages/api/store/storeUser.ts
@@ -1,35 +1,35 @@
-import postgres from 'postgres';
 import { NextApiRequest, NextApiResponse } from 'next';
-let sql: postgres.Sql;
+import prisma from '../../../lib/prisma';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (process.env.DATABASE_URL) {
-    sql = postgres(process.env.DATABASE_URL, { ssl: 'require' });
-  } else {
-    throw new Error('DATABASE_URL environment variable is not defined.');
-  }
-
   if (req.method === 'PUT') {
     const { clerkUserID, username, email } = req.body;
-    const userExists = await sql`
-      SELECT EXISTS(SELECT 1 FROM users WHERE user_id = ${clerkUserID});
-    `;
-    if (userExists[0].exists) {
+
+    const userExists = await prisma.users.findUnique({
+      where: { user_id: clerkUserID },
+    });
+
+    if (userExists) {
       res.status(409).json({ error: 'User already exists' });
       return;
     }
-    try {
-      await sql`
-    INSERT INTO users (user_id, username, email)
-    VALUES (${clerkUserID}, ${username}, ${email});
-  `;
 
-      res
-        .status(201)
-        .json({ message: 'User created successfully', user_id: clerkUserID });
+    try {
+      const newUser = await prisma.users.create({
+        data: {
+          user_id: clerkUserID,
+          username: username,
+          email: email,
+        },
+      });
+
+      res.status(201).json({
+        message: 'User created successfully',
+        user_id: newUser.user_id,
+      });
     } catch (error) {
       console.error('Error inserting user:', error);
       res.status(500).json({ error: 'Error creating user' });
