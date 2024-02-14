3fa83dfeb1f6c7782dfd69182b38126726915c87
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-02-14 11:57:47 +0000

    update [student_id_bookings].tsx to use getserversideprops

--- pages/bookings/[student_id_bookings].tsx
+++ pages/bookings/[student_id_bookings].tsx
@@ -9,56 +9,46 @@ import {
 	DialogTrigger,
 } from "@/components/ui/dialog";
 import { Separator } from "@/components/ui/separator";
-import { SignedIn, SignedOut, useAuth } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { students } from "@prisma/client";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import type { GetServerSideProps } from "next";
+import prisma from "@/lib/prisma";
+import { getAuth } from "@clerk/nextjs/server";
 
-function Bookings() {
-	const { userId } = useAuth();
+interface BookingData {
+	booking_id: number;
+	student_id: number;
+	user_id: string;
+	selected_class: string;
+	created_at: string;
+	student: students;
+}
+
+function Bookings({
+	student_id_bookings,
+	student_info,
+}: { student_id_bookings: BookingData[]; student_info: students }) {
 	const router = useRouter();
-	const { student_id_bookings } = router.query;
+	const [deleteDialogOpen, setDeleteDialogOpen] = useState<boolean[]>(
+		new Array(student_id_bookings.length).fill(false),
+	);
 
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	const [bookings, setBookings] = useState<any[]>([]);
-	const [studentInfo, setStudentInfo] = useState<students>();
-	const [deleteDialogOpen, setDeleteDialogOpen] = useState<boolean[]>([]);
-	const [isLoading, setIsLoading] = useState(true);
-
-	async function fetchBookings() {
-		try {
-			const data = await fetch(
-				`/api/fetch/getBookingsFromIds?user_id=${userId}&student_id=${student_id_bookings}`,
-			);
-			const bookings = await data.json();
-			setBookings(bookings);
-			setDeleteDialogOpen(new Array(bookings.length).fill(false));
-		} catch (error) {
-			console.error("Error fetching data:", error);
-		}
-	}
-
-	async function lookupStudent() {
-		try {
-			const data = await fetch(
-				`/api/fetch/getStudentFromStudentID?student_id=${student_id_bookings}&user_id=${userId}`,
-			);
-			return await data.json();
-		} catch (error) {
-			console.error("Error fetching data:", error);
-		}
-	}
+	const refreshData = () => {
+		void router.replace(router.asPath);
+	};
 
 	async function removeBooking(booking_id: string, index: number) {
-		fetch(`/api/delete/deleteBooking?booking_id=${booking_id}`, {
+		void fetch(`/api/delete/deleteBooking?booking_id=${booking_id}`, {
 			method: "DELETE",
 			headers: {
 				"Content-Type": "application/json",
 			},
 		}).then(async (response) => {
 			if (response.ok) {
-				await fetchBookings();
+				refreshData();
 				const newDeleteDialogOpen = [...deleteDialogOpen];
 				newDeleteDialogOpen[index] = false;
 				setDeleteDialogOpen(newDeleteDialogOpen);
@@ -68,23 +58,6 @@ function Bookings() {
 		});
 	}
 
-	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-	useEffect(() => {
-		if (student_id_bookings) {
-			fetchBookings()
-				.then(() => {
-					lookupStudent().then((student) => {
-						setStudentInfo(student);
-					});
-					setIsLoading(false);
-				})
-				.catch((error) => {
-					console.error("Error fetching bookings:", error);
-					setIsLoading(false);
-				});
-		}
-	}, [userId, student_id_bookings]);
-
 	return (
 		<>
 			<Head>
@@ -93,26 +66,10 @@ function Bookings() {
 			</Head>
 			<SignedIn>
 				<br />
-				{isLoading ? (
-					<p>Loading...</p>
-				) : (
-					<>
-						{bookings && bookings.length > 0 ? (
-							<>
-								<h1 className="text-3xl font-semibold text-gray-900 dark:text-gray-50 flex justify-center">
-									{bookings[0].student.student_first_name}{" "}
-									{bookings[0].student.student_last_name}&apos;s Bookings
-								</h1>
-							</>
-						) : (
-							<h1 className="text-3xl font-semibold text-gray-900 dark:text-gray-50 flex justify-center">
-								{studentInfo
-									? `No bookings found for ${studentInfo.student_first_name} ${studentInfo.student_last_name}`
-									: "Student not found"}
-							</h1>
-						)}
-					</>
-				)}
+				<h1 className="text-3xl font-semibold text-gray-900 dark:text-gray-50 flex justify-center">
+					{student_info.student_first_name} {student_info.student_last_name}
+					&apos;s Bookings
+				</h1>
 				<br />
 				<Separator />
 				<Button
@@ -129,10 +86,9 @@ function Bookings() {
 				</Button>
 				<br />
 				<div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-5 p-8">
-					{bookings ? (
-						bookings.map((booking, index) => (
-							// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-							<Card key={index}>
+					{student_id_bookings ? (
+						student_id_bookings.map((booking) => (
+							<Card key={booking.booking_id}>
 								<CardHeader>
 									<CardTitle>
 										<p>
@@ -148,10 +104,14 @@ function Bookings() {
 									</p>
 									<br />
 									<Dialog
-										open={deleteDialogOpen[index]}
+										open={
+											deleteDialogOpen[student_id_bookings.indexOf(booking)]
+										}
 										onOpenChange={(isOpen) => {
 											const newDeleteDialogOpen = [...deleteDialogOpen];
-											newDeleteDialogOpen[index] = isOpen;
+											newDeleteDialogOpen[
+												student_id_bookings.indexOf(booking)
+											] = isOpen;
 											setDeleteDialogOpen(newDeleteDialogOpen);
 										}}
 									>
@@ -176,7 +136,10 @@ function Bookings() {
 												variant={"destructive"}
 												size={"lg"}
 												onClick={() => {
-													removeBooking(booking.booking_id, index);
+													void removeBooking(
+														booking.booking_id.toString(10),
+														student_id_bookings.indexOf(booking),
+													);
 												}}
 											>
 												Remove Class Booking
@@ -208,5 +171,76 @@ function Bookings() {
 		</>
 	);
 }
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const student_id = parseInt(
+		context.params?.student_id_bookings as string,
+		10,
+	);
+	const { userId } = getAuth(context.req);
+
+	if (!userId) {
+		return {
+			redirect: {
+				destination: "/sign-in",
+				permanent: false,
+			},
+		};
+	}
+	if (!student_id) {
+		return {
+			redirect: {
+				destination: "/students",
+				permanent: false,
+			},
+		};
+	}
+
+	const bookings = await prisma.bookings.findMany({
+		where: {
+			user_id: userId,
+			student_id: student_id,
+		},
+		include: {
+			student: true,
+		},
+	});
+
+	const student = await prisma.students.findUnique({
+		where: {
+			student_id: student_id,
+			user_id: userId,
+		},
+	});
+
+	if (!bookings || !student) {
+		return {
+			redirect: {
+				destination: "/students",
+				permanent: false,
+			},
+		};
+	}
+
+	const bookingsFormattedDate = bookings.map((booking) => ({
+		...booking,
+		created_at: booking.created_at.toISOString(),
+		student: {
+			...booking.student,
+			student_dob: booking.student.student_dob.toISOString(),
+		},
+	}));
+
+	const studentFormattedDate = {
+		...student,
+		student_dob: student.student_dob.toISOString(),
+	};
+
+	return {
+		props: {
+			student_id_bookings: bookingsFormattedDate,
+			student_info: studentFormattedDate,
+		},
+	};
+};
 
 export default Bookings;
