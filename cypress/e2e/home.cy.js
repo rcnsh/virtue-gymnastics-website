e6ea08a7222391ad2e63395b91e6426c44de32a4
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-04-23 14:31:18 +0100

    add back sign in logic for test

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,4 +1,18 @@
 describe("Home page testing", () => {
+	beforeEach(() => {
+		cy.visit("/", { failOnStatusCode: false });
+		cy.clearCookies({ domain: window.location.domain });
+		cy.reload();
+		cy.contains("Login").click();
+		cy.get(".cl-formFieldInput__identifier")
+			.should("be.visible")
+			.type(Cypress.env("EMAIL"));
+		cy.get(".cl-formButtonPrimary").click();
+		cy.get(".cl-formFieldInput__password")
+			.should("be.visible")
+			.type(Cypress.env("PASSWORD"));
+		cy.get(".cl-formButtonPrimary").click();
+	});
 	it("Clicks on the events link on the home page", () => {
 		cy.visit("/", { failOnStatusCode: false });
 		cy.get("#footer-section").contains("Events/Camps").click();
