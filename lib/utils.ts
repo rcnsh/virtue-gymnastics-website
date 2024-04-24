26af470fe722be93c7e4fca36f60c407d6fc3ab6
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-04-24 09:48:20 +0100

    add commenting to utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,10 +4,21 @@ import { twMerge } from "tailwind-merge";
 import prisma from "./prisma";
 import type { users } from "@prisma/client";
 
+/**
+ * Combines multiple class names into a single string.
+ * @param inputs - The class names to be combined.
+ * @returns The combined class names as a string.
+ */
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
+/**
+ * Checks if a user is an admin.
+ * @param userId - The ID of the user to check.
+ * @param res - The response object to send the result to.
+ * @returns A Promise that resolves to void.
+ */
 export async function isUserAdmin(
 	userId: string,
 	res: NextApiResponse,
